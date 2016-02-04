8672200a4b4b19010b50ae3aceeaaac06bd81905
Brett <brettrheenan@gmail.com>
2016-02-04 19:51:48 +0100

    finished pedal and amp functionality

--- app/controllers/amps_controller.rb
+++ app/controllers/amps_controller.rb
@@ -1,2 +1,63 @@
 class AmpsController < ApplicationController
+
+	get '/amps' do
+		if logged_in?
+			@users = User.all
+			erb :'amps/index'
+		else
+			login_failure
+		end
+	end
+
+	get '/amps/new' do
+		if logged_in?
+			erb :'amps/new'
+		else
+			login_failure
+		end
+	end
+
+	post '/amps' do
+		if current_user.amps.create(params).save
+			redirect '/users/show'
+		else
+			@error = "Please fill in all fields"
+			erb :'amps/new'
+		end
+	end
+
+	get '/amps/:id/edit' do
+		if logged_in?
+			@amp = Amp.find_by_id(params[:id])
+			erb :'amps/edit'
+		else
+			login_failure
+		end
+	end
+
+	delete '/amps/:id/delete' do
+		@amp = Amp.find_by_id(params[:id])
+		@amp.destroy if @amp.user_id == current_user.id
+		@error = 'amp successfully deleted'
+		erb :'/users/show'
+	end
+
+	patch '/amps/:id' do
+		@amp = Amp.find_by_id(params[:id])
+		if @amp.update(params[:amp])
+			redirect '/amps'
+		else
+			@error = "Please fill in all fields"
+			erb :"amps/edit"
+		end
+	end
+
+	get '/amps/:id' do
+		if logged_in?
+			@amp = Amp.find_by_id(params[:id])
+			erb :'amps/show'
+		else
+			login_failure
+		end
+	end
 end
--- app/controllers/pedals_controller.rb
+++ app/controllers/pedals_controller.rb
@@ -1,2 +1,63 @@
 class PedalsController < ApplicationController
+
+	get '/pedals' do
+		if logged_in?
+			@users = User.all
+			erb :'pedals/index'
+		else
+			login_failure
+		end
+	end
+
+	get '/pedals/new' do
+		if logged_in?
+			erb :'pedals/new'
+		else
+			login_failure
+		end
+	end
+
+	post '/pedals' do
+		if current_user.pedals.create(params).save
+			redirect '/users/show'
+		else
+			@error = "Please fill in all fields"
+			erb :'pedals/new'
+		end
+	end
+
+	get '/pedals/:id/edit' do
+		if logged_in?
+			@pedal = Pedal.find_by_id(params[:id])
+			erb :'pedals/edit'
+		else
+			login_failure
+		end
+	end
+
+	delete '/pedals/:id/delete' do
+		@pedal = Pedal.find_by_id(params[:id])
+		@pedal.destroy if @pedal.user_id == current_user.id
+		@error = 'Pedal successfully deleted'
+		erb :'/users/show'
+	end
+
+	patch '/pedals/:id' do
+		@pedal = Pedal.find_by_id(params[:id])
+		if @pedal.update(params[:pedal])
+			redirect '/pedals'
+		else
+			@error = "Please fill in all fields"
+			erb :"pedals/edit"
+		end
+	end
+
+	get '/pedals/:id' do
+		if logged_in?
+			@pedal = Pedal.find_by_id(params[:id])
+			erb :'pedals/show'
+		else
+			login_failure
+		end
+	end
 end
