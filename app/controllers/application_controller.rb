2802a81a895fa8c10506b9b20c2568fa764427d5
Brett <brettrheenan@gmail.com>
2016-02-05 17:14:57 +0100

    finished layout and updated functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < Sinatra::Base
   configure do
   	enable :sessions
     set :session_secret, "guitar_hero"
-    set :public_folder, 'public'
+    set :public_folder, 'app/public'
     set :views, 'app/views'
   end
 
